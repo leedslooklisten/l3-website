d6860a3ac90abef8129acbff6c7653229e4ab1b5
Greg Sakorafis <gsakorafis@gmail.com>
2018-08-07 00:51:54 -0700

    Desktop nav bar styling and placeholder buttons

--- src/App.js
+++ src/App.js
@@ -2,12 +2,52 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function HamburgerNav(props) {
+  return (
+    <div class="hamburgerNav" />
+  );
+}
+
+class MenuBarButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="menuBarButton"><div>{this.props.label}</div></div>
+    );
+  }
+}
+
+class MenuBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="menuBar">
+        <div class="leftMenuBarButtons">
+          <HamburgerNav />
+          <MenuBarButton label='home' />
+          <MenuBarButton label='step into the studio' />
+          <MenuBarButton label='our story' />
+        </div>
+        <div class="menuBarLogo" />
+        <div class="rightMenuBarButtons">
+          <div class="placeholderRightButton" />
+        </div>
+      </div>
+    )
+  }
+}
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hello, world!
-      </div>
+      <MenuBar />
     );
   }
 }
