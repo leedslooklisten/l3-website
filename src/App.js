11902d8aa80d6b5bffb6a15993efacafd4b6b22b
Greg Sakorafis <gsakorafis@gmail.com>
2018-08-07 01:02:04 -0700

    Refactor MenuBar into a separate file group

--- src/App.js
+++ src/App.js
@@ -1,47 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MenuBar from './MenuBar';
 
-function HamburgerNav(props) {
-  return (
-    <div class="hamburgerNav" />
-  );
-}
-
-class MenuBarButton extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div class="menuBarButton"><div>{this.props.label}</div></div>
-    );
-  }
-}
-
-class MenuBar extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div class="menuBar">
-        <div class="leftMenuBarButtons">
-          <HamburgerNav />
-          <MenuBarButton label='home' />
-          <MenuBarButton label='step into the studio' />
-          <MenuBarButton label='our story' />
-        </div>
-        <div class="menuBarLogo" />
-        <div class="rightMenuBarButtons">
-          <div class="placeholderRightButton" />
-        </div>
-      </div>
-    )
-  }
-}
 
 
 class App extends Component {
---
+++ src/MenuBar.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import './MenuBar.css';
+
+function HamburgerNav(props) {
+  return (
+    <div class="hamburgerNav" />
+  );
+}
+
+class MenuBarButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="menuBarButton"><div>{this.props.label}</div></div>
+    );
+  }
+}
+
+class MenuBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="menuBar">
+        <div class="leftMenuBarButtons">
+          <HamburgerNav />
+          <MenuBarButton label='home' />
+          <MenuBarButton label='step into the studio' />
+          <MenuBarButton label='our story' />
+        </div>
+        <div class="menuBarLogo" />
+        <div class="rightMenuBarButtons">
+          <div class="placeholderRightButton" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MenuBar;
