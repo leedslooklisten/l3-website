7af26b34a8723186ac5cb8208f273049d2b1f448
J. Benjamin Leeds <leeds@uw.edu>
2018-09-11 18:36:43 -0700

    resolve video tag attribute autoplay should be autoPlay compiler error

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,15 +1,18 @@
 import React from "react";
 
+// TODO compiler requires "autoPlay" attribute, HTML 5 spec lists: "autoplay"
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/video#Attributes
+
+// TODO compiler requires </source> closing tag, HTML 5 spec defines no closing tag for <source>
+// https://developer.mozilla.org/en-US/docs/Web/HTML/Element/source
+
 const VideoPlayer = () => {
     return (
         <div class="fancyVideo">
-            <video 
-            // src="https://player.vimeo.com/external/260015882.hd.mp4?s=a1321e6df42f072dc1a0c62018580a2b2387f402&amp;profile_id=174"
-            src="https://storage.googleapis.com/leedssound.com/Leeds_Studio7_LaunchVideo%5B1080pWEB%5D.mp4" 
-            autoplay="autoplay" 
-            loop="loop" 
-            preload="auto" 
-            muted="true" />
+            <video autoPlay loop="loop" preload="auto" muted="true">
+                <source src="https://storage.googleapis.com/leedssound.com/Leeds_Studio7_LaunchVideo%5B1080pWEB%5D.mp4" type="video/mp4"/> 
+                Your browser does not support the HTML 5 video tag.
+            </video>
         </div>
     );
 };
