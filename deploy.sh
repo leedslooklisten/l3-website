dc4154cf4088d1e94bf5de694e920a2762157c9f
J. Benjamin Leeds <leeds@uw.edu>
2018-09-11 19:26:27 -0700

    update deploy.sh script to restart GCE VM with new image

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!bin/bash
 
+if [ "$1" = "" ]; then
+    echo "Usage: sh deploy.sh [deployment version]"
+    exit
+fi
+
 # set environment
 # export env=dockerdev
 
@@ -10,17 +15,18 @@ GOOS=linux go build # build Golang webserver
 # delete old image
 docker stop ls-website
 docker rm ls-website
-docker rmi us.gcr.io/leedslooklisten-com/ls-website:0.2
+docker rmi us.gcr.io/leedslooklisten-com/ls-website:$1
 
 # create docker image
-docker build -t us.gcr.io/leedslooklisten-com/ls-website:0.3 .
+docker build -t us.gcr.io/leedslooklisten-com/ls-website:$1 .
 
 # run docker dev
 # docker run --name ls-website -e "env=dockerdev" -p 8080:80 us.gcr.io/leedslooklisten-com/ls-website:0.1
 
 # run prod test
-docker run --name ls-website -e "env=beta" -p 8080:80 us.gcr.io/leedslooklisten-com/ls-website:0.3
+# docker run --name ls-website -e "env=beta" -p 8080:80 us.gcr.io/leedslooklisten-com/ls-website:$1
 
 # deploy to Gcloud
 
-# docker push us.gcr.io/leedslooklisten-com/ls-website:0.3
+docker push us.gcr.io/leedslooklisten-com/ls-website:$1
+gcloud compute instances update-container ls-website --container-image=us.gcr.io/leedslooklisten-com/ls-website:$1
